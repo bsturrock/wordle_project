fef9c5f4a5b511b79bb98eea55d87b395a76fe49
Brandon Sturrock <brandon.sturrock@outlook.com>
2023-10-23 15:46:26 -0400

    functionality for typing a guess and displaying the guess

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+const body = document.querySelector('body') // body element of DOM
+
+const guess = []
+const round = 1 // basically which attempt the user is on (from 1 to 6)
+
+body.addEventListener('keydown', (event) => {
+    if(event.keyCode >= 65 && event.keyCode <= 90 && guess.length < 5) { // if keydown is between A and Z keys and word isn't 5 letters
+        guess.push(event.key)
+        display_guessed_word()
+    } else if(event.key == 'Backspace' && guess.length > 0){
+        guess.pop()
+        display_guessed_word()
+    }
+})
+
+function display_guessed_word(){
+    const guess_copy = [...guess] // copy of the guess array
+    
+    while(guess_copy.length < 5){
+        guess_copy.push('') // add blanks to fill up 5 elements in the array
+    }
+
+    //fill the current row boxes with letters
+    document.querySelector(`#one_${round}`).innerText = guess_copy[0]
+    document.querySelector(`#two_${round}`).innerText = guess_copy[1]
+    document.querySelector(`#three_${round}`).innerText = guess_copy[2]
+    document.querySelector(`#four_${round}`).innerText = guess_copy[3]
+    document.querySelector(`#five_${round}`).innerText = guess_copy[4]
+}
