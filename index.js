145a4260ce972886c6c9fc4d09a3532272b3cdfe
Miguel-Val <chahpo@gmail.com>
2023-10-27 05:53:44 -0400

    buttons logic finished needs style

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const body = document.querySelector("body"); // body element of DOM
-const leaderboard_btn = document.querySelector('leaderboard-btn');
-const leaderboard_container = document.getElementById('leaderboard-container');
-const leaderboard_close = document.getElementById('leaderboard-close');
+const btn_container = document.querySelector(".btn-container");
+const how_to_play_btn = document.querySelector(".question-btn");
+const how_to_play_container = document.getElementById("how-to-play-container");
+const leaderboard_btn = document.querySelector(".leaderboard-btn");
+const leaderboard_container = document.getElementById("leaderboard-container");
+const close_btn = document.querySelectorAll(".close");
 const guess = []; // current word the user is guessing (all letters are individual array elements)
 const target_guess = []; // current word the user is trying to guess (all letters are individual array elements)
 let round = 1; // what row/guess the user is currently on
@@ -14,21 +17,61 @@ let current_row = [
     document.querySelector(`#five_${round}`),
 ];
 
-// leaderboard_btn.addEventListener('click', () => {
-//     document.querySelector('.btn-container').style.display = 'block';
-// });
-// leaderboard_close.addEventListener('click', () => {
-//     document.querySelector('.btn-container').style.display = 'none';
-// });
+let text = ["s", "p", "e", "n", "t"];
+const typing_Text = [];
+let current_Index = 0;
+let animation_Interval;
 
-// document.querySelectorAll(".close").forEach(() =>{
-//     close.style.display = 'none';
-// })
+for (let i = 1; i <= text.length; i++) {
+    typing_Text.push(document.getElementById(`typing-text-${i}`));
+}
 
+function typeTextElement(element) {
+    if (current_Index >= text.length) {
+        current_Index = 0;
+        typing_Text.forEach((element) => (element.innerHTML = ""));
+    }
 
+    element.innerHTML += text[current_Index];
+    current_Index++;
+}
+
+function start_Typing_Animation() {
+    animation_Interval = setInterval(function () {
+        typeTextElement(typing_Text[current_Index]);
+    }, 600);
+    setTimeout(function () {
+        clearInterval(animation_Interval);
+        typing_Text[0].classList.add = ('green');
+    }, 3500);
+}
+
+function stop_Typing_Animation() {
+    clearInterval(animation_Interval);
+    current_Index = 0;
+    typing_Text.forEach((element) => (element.innerHTML = ""));
+}
+
+how_to_play_btn.addEventListener("click", () => {
+    how_to_play_container.style.display = "block";
+    start_Typing_Animation();
+});
+
+leaderboard_btn.addEventListener("click", () => {
+    leaderboard_container.style.display = "block";
+});
+
+close_btn.forEach((closeElement) => {
+    closeElement.addEventListener("click", () => {
+        const container = closeElement.closest(".btn-container");
+        if (container) {
+            container.style.display = "none";
+            stop_Typing_Animation();
+        }
+    });
+});
 
 body.addEventListener("keydown", (event) => {
-
     if (event.code.slice(0, 3) == "Key" && guess.length < 5) {
         // if keydown is between A and Z keys and word isn't 5 letters
         guess.push(event.key);
@@ -37,8 +80,8 @@ body.addEventListener("keydown", (event) => {
         guess.pop();
         display_guessed_word();
     } else if (event.code == "Enter" && guess.length == 5) {
-        calculate()
-        new_round()
+        calculate();
+        new_round();
     }
 });
 
@@ -47,66 +90,68 @@ document.querySelectorAll(".letter").forEach((ele) => {
     ele.addEventListener("animationend", remove_all_animation_classes);
 });
 
-
-function new_round(){
-    round++
-    current_row = update_current_row()
-    guess.length = 0
+function new_round() {
+    round++;
+    current_row = update_current_row();
+    guess.length = 0;
 }
 
-function calculate(){
+function calculate() {
+    const colors = [];
+    let correct = 0;
 
-    const colors = []
-    let correct = 0
+    let target_letter_occurrences = letter_occurrences();
+    let greens = green_instances();
 
-    let target_letter_occurrences = letter_occurrences()
-    let greens = green_instances()
-
-
-// First loop to look for correct spots and letters not in the word
-    for(let index in guess){ 
-        if(!target_guess.includes(guess[index])){ // target word does not include letter
-            colors[index] = 'gray'
-        } else if(target_guess[index] == guess[index]){ // letter in right spot
-            colors[index] = 'green'
-            ++correct
-            ++greens[guess[index]]    
+    // First loop to look for correct spots and letters not in the word
+    for (let index in guess) {
+        if (!target_guess.includes(guess[index])) {
+            // target word does not include letter
+            colors[index] = "gray";
+        } else if (target_guess[index] == guess[index]) {
+            // letter in right spot
+            colors[index] = "green";
+            ++correct;
+            ++greens[guess[index]];
         } else {
-            colors[index] = '' // placeholder for any letter not meeting these critera
+            colors[index] = ""; // placeholder for any letter not meeting these critera
         }
     }
 
-// Second loop to deal with incorrect positions / duplicate letters
-    for(let index in guess){
-        if(colors[index]!='green' && colors[index]!='gray'){
-            if(target_letter_occurrences[guess[index]] == greens[guess[index]]){
-                colors[index]='gray'
+    // Second loop to deal with incorrect positions / duplicate letters
+    for (let index in guess) {
+        if (colors[index] != "green" && colors[index] != "gray") {
+            if (
+                target_letter_occurrences[guess[index]] == greens[guess[index]]
+            ) {
+                colors[index] = "gray";
             } else {
-                colors[index]= 'yellow'
+                colors[index] = "yellow";
             }
         }
     }
 
-    current_row[0].classList.add(colors[0])
-    current_row[1].classList.add(colors[1])
-    current_row[2].classList.add(colors[2])
-    current_row[3].classList.add(colors[3])
-    current_row[4].classList.add(colors[4])
-
+    current_row[0].classList.add(colors[0]);
+    current_row[1].classList.add(colors[1]);
+    current_row[2].classList.add(colors[2]);
+    current_row[3].classList.add(colors[3]);
+    current_row[4].classList.add(colors[4]);
 }
 
-function green_instances(){ // initializes an object to track how many 'green' instances per letter there are
-    const instances = {}
-    for(let letter of guess){
-        instances[letter] = 0
+function green_instances() {
+    // initializes an object to track how many 'green' instances per letter there are
+    const instances = {};
+    for (let letter of guess) {
+        instances[letter] = 0;
     }
-    return instances
+    return instances;
 }
 
-function letter_occurrences(){ // creates an object that counts how many instances of each letter there is in the target word
-    return target_guess.reduce((acc, curr)=>{
-        return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
-    },{})
+function letter_occurrences() {
+    // creates an object that counts how many instances of each letter there is in the target word
+    return target_guess.reduce((acc, curr) => {
+        return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
+    }, {});
 }
 
 function remove_all_animation_classes() {
@@ -171,19 +216,16 @@ function update_current_row() {
 }
 
 function get_random_word(target_arry) {
-    
     // this api is a random word generator which fetches a random word with the length of 5
-    fetch('https://random-word-api.herokuapp.com/word?length=5')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        for(const letter of data[0]){ 
-            target_arry.push(letter);
-        } 
-        
-    })
-
+    fetch("https://random-word-api.herokuapp.com/word?length=5")
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            for (const letter of data[0]) {
+                target_arry.push(letter);
+            }
+        });
 
     //IF YOU WANT TO ADD THE WORD TO A LOCAL STORAGE AND DO SOMETHING WITH IT REPLACE THIS.
 
@@ -191,9 +233,9 @@ function get_random_word(target_arry) {
     // if (stored_random_word) {
     //     for(const letter of stored_random_word){ // pushes the random word into the "guess" array
     //         target_arry.push(letter);
-    //     } 
+    //     }
     // } else {
-    //     
+    //
     //     fetch("https://random-word-api.herokuapp.com/word?length=5")
     //         .then((Response) => {
     //             return Response.json(); // converts this to a javascript array
@@ -203,10 +245,6 @@ function get_random_word(target_arry) {
     //             localStorage.setItem("randomWord", data[0]);
     //         });
     // }
-
 }
 
 get_random_word(target_guess);
-
-
-
