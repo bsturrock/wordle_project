1e9db2258fee403d63633258f90e4e17a37c5289
Miguel-Val <chahpo@gmail.com>
2023-10-30 19:34:53 -0400

    finished how to play button

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const how_to_play_btn = document.querySelector(".question-btn");
 const how_to_play_container = document.getElementById("how-to-play-container");
 const leaderboard_btn = document.querySelector(".leaderboard-btn");
 const leaderboard_container = document.getElementById("leaderboard-container");
+const typingText1 = document.getElementById("typing-text-1");
+const typingText7 = document.getElementById("typing-text-7");
 const close_btn = document.querySelectorAll(".close");
 const guess = []; // current word the user is guessing (all letters are individual array elements)
 const target_guess = []; // current word the user is trying to guess (all letters are individual array elements)
-
-let ready_to_guess = true
+console.log(target_guess);
+let ready_to_guess = true;
 
 let round = 1; // what row/guess the user is currently on
 let current_row = [
@@ -20,15 +22,40 @@ let current_row = [
     document.querySelector(`#five_${round}`),
 ];
 
-let text = ["W", "A", "T", "E", "R","E","A","R","T","H","A","P","P","L","E"];
+// Create an array of text to be displayed
+const text = [
+    "W",
+    "A",
+    "T",
+    "E",
+    "R",
+    "E",
+    "A",
+    "R",
+    "T",
+    "H",
+    "A",
+    "P",
+    "P",
+    "L",
+    "E",
+];
+
+// Create an array to hold HTML elements for typing text
 const typing_Text = [];
+
+// Initialize the current index for typing
 let current_Index = 0;
+
+// Declare a variable for the animation interval
 let animation_Interval;
 
+// Loop to populate the typing_Text array with HTML elements
 for (let i = 1; i <= text.length; i++) {
     typing_Text.push(document.getElementById(`typing-text-${i}`));
 }
 
+// Function to type text in the HTML elements
 function typeTextElement(element) {
     if (current_Index >= text.length) {
         current_Index = 0;
@@ -39,31 +66,69 @@ function typeTextElement(element) {
     current_Index++;
 }
 
+// Function to start the typing animation
 function start_Typing_Animation() {
     animation_Interval = setInterval(function () {
         typeTextElement(typing_Text[current_Index]);
     }, 300);
-    setTimeout(function () {
+
+    // Set a timeout to stop the animation after a certain duration
+    timeout = setTimeout(function () {
         clearInterval(animation_Interval);
-        
-    }, 4500);
+
+        // Modify the appearance of specific elements
+        typing_Text[0].style.backgroundColor = "green";
+        typing_Text[0].style.color = "white";
+        typing_Text[6].style.backgroundColor = "goldenrod";
+        typing_Text[6].style.color = "white";
+
+        // Add the "fade" class and remove elements at specific indices
+        typing_Text[0].classList.add("fade");
+        typing_Text[6].classList.add("fade");
+        typing_Text.splice(6, 1);
+        typing_Text.splice(0, 1);
+
+        // Reset the appearance of all elements in the array
+        typing_Text.forEach((element) => {
+            element.classList.add("fade");
+            element.style.backgroundColor = "#3c3c3c";
+            element.style.color = "white";
+        });
+    }, 4790);
 }
 
+// Function to stop the typing animation
 function stop_Typing_Animation() {
+    // Clear the animation interval and the timeout
     clearInterval(animation_Interval);
+    clearTimeout(timeout);
+
+    // Restore elements at specific indices
+    typing_Text.splice(5, 0, typingText7);
+    typing_Text.splice(0, 0, typingText1);
+
+    // Reset the current index and appearance of all elements
     current_Index = 0;
-    typing_Text.forEach((element) => (element.innerHTML = ""));
+    typing_Text.forEach((element) => {
+        element.classList.remove("fade");
+        element.style.backgroundColor = "white";
+        element.style.color = "black";
+        element.innerHTML = "";
+    });
 }
 
+// Event listener for "How to Play" button
 how_to_play_btn.addEventListener("click", () => {
     how_to_play_container.style.display = "block";
     start_Typing_Animation();
 });
 
+// Event listener for "Leaderboard" button
 leaderboard_btn.addEventListener("click", () => {
     leaderboard_container.style.display = "block";
 });
 
+// Event listeners for "Close" buttons
 close_btn.forEach((closeElement) => {
     closeElement.addEventListener("click", () => {
         const container = closeElement.closest(".btn-container");
@@ -75,7 +140,6 @@ close_btn.forEach((closeElement) => {
 });
 
 body.addEventListener("keydown", (event) => {
-
     if (event.code.slice(0, 3) == "Key" && guess.length < 5) {
         // if keydown is between A and Z keys and word isn't 5 letters
         guess.push(event.key);
@@ -107,14 +171,12 @@ function new_round() {
     guess.length = 0;
 }
 
-function calculate(){
+function calculate() {
+    const colors = [];
+    let correct = 0;
 
-    const colors = []
-    let correct = 0
-
-    let target_letter_occurrences = letter_occurrences()
-    let greens = green_instances()
-
+    let target_letter_occurrences = letter_occurrences();
+    let greens = green_instances();
 
     // First loop to look for correct spots and letters not in the word
     for (let index in guess) {
@@ -144,12 +206,11 @@ function calculate(){
         }
     }
 
-    current_row[0].classList.add(colors[0])
-    current_row[1].classList.add(colors[1])
-    current_row[2].classList.add(colors[2])
-    current_row[3].classList.add(colors[3])
-    current_row[4].classList.add(colors[4])
-
+    current_row[0].classList.add(colors[0]);
+    current_row[1].classList.add(colors[1]);
+    current_row[2].classList.add(colors[2]);
+    current_row[3].classList.add(colors[3]);
+    current_row[4].classList.add(colors[4]);
 }
 
 function green_instances() {
@@ -168,8 +229,6 @@ function letter_occurrences() {
     }, {});
 }
 
-
-
 function display_guessed_word(animate = false) {
     const guess_copy = [...guess]; // copy of the guess array
 
@@ -178,27 +237,27 @@ function display_guessed_word(animate = false) {
     }
 
     //fill the current row boxes with letters and add animation class if required
-    current_row[0].querySelector('span').innerText = guess_copy[0];
+    current_row[0].querySelector("span").innerText = guess_copy[0];
     if (guess.length == 1 && animate) {
         document.querySelector(`#one_${round}`).classList.add("bounce");
     }
 
-    current_row[1].querySelector('span').innerText = guess_copy[1];
+    current_row[1].querySelector("span").innerText = guess_copy[1];
     if (guess.length == 2 && animate) {
         document.querySelector(`#two_${round}`).classList.add("bounce");
     }
 
-    current_row[2].querySelector('span').innerText = guess_copy[2];
+    current_row[2].querySelector("span").innerText = guess_copy[2];
     if (guess.length == 3 && animate) {
         document.querySelector(`#three_${round}`).classList.add("bounce");
     }
 
-    current_row[3].querySelector('span').innerText = guess_copy[3];
+    current_row[3].querySelector("span").innerText = guess_copy[3];
     if (guess.length == 4 && animate) {
         document.querySelector(`#four_${round}`).classList.add("bounce");
     }
 
-    current_row[4].querySelector('span').innerText = guess_copy[4];
+    current_row[4].querySelector("span").innerText = guess_copy[4];
     if (guess.length == 5 && animate) {
         document.querySelector(`#five_${round}`).classList.add("bounce");
     }
